e62756a82dd1055fd628357222b078b20e52216f
Sankalp7317 <148464467+Sankalp7317@users.noreply.github.com>
2026-06-27 15:45:12 +0530

    product view

---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import useMacbookStore from "../store/index.js";
+import clsx from "clsx";
+
+const ProductViewer = () => { const {color,scale,setColor,setScale} = useMacbookStore()
+    return (
+        <section id="product-viewer">
+            <h2>Take a closer look.</h2>
+
+            <div className="controls">
+                <p className="info">MacbookPro {scale}" in {color}</p>
+
+                <div className="flex-center gap-5 mt-5">
+                    <div className="color-control">
+                        <div
+                            onClick={() => setColor('#adb5bd')}
+                            className={clsx('bg-neutral-300',color==='#adb5bd'&&'active')}
+                        />
+                        <div
+                            onClick={() => setColor('#2e2c2e')}
+                            className={clsx('bg-neutral-900',color==='#2e2c2e'&&'active')}
+                        />
+                    </div>
+
+                    <div className="size-control">
+                        <div
+                            onClick={() => setScale(0.06)}
+                            className={clsx(scale === 0.06 ? 'bg-white text-black':'bg-transparent text-white')}
+                        >
+                            14"
+                        </div>
+                        <div
+                            onClick={() => setScale(0.08)}
+                            className={clsx(scale === 0.08 ? 'bg-white text-black':'bg-transparent text-white')}
+                        >
+                            16"
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+export default ProductViewer
