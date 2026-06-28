8af08d512d191b335de434919f32c207b86a3e3d
Sankalp7317 <148464467+Sankalp7317@users.noreply.github.com>
2026-06-28 09:39:25 +0530

    work on models 2

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,6 +3,7 @@ import useMacbookStore from "../store/index.js";
 import clsx from "clsx";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14.jsx";
 
 const ProductViewer = () => { const {color,scale,setColor,setScale} = useMacbookStore()
     return (
@@ -41,8 +42,9 @@ const ProductViewer = () => { const {color,scale,setColor,setScale} = useMacbook
                 </div>
             </div>
             <Canvas id="canvas" camera={{position:[0,2,5],fov:50,near:.1,far:100}}>
-                <Box position={[0,0,0]} scale={10*scale} material-color={color}></Box>
-
+                {/*<Box position={[0,0,0]} scale={10*scale} material-color={color}/>*/}
+                <MacbookModel14 scale={0.06} position={[0,0,0]}/>
+                <ambientLight intensity={20} />
                 <OrbitControls enableZoom={false}/>
             </Canvas>
         </section>
